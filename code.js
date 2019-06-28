d6ac90d6513bdcb19e40f3261c45ff7a1cec48cd
Vedernikov Ivan <ivedernikov@ivedernikov-MacBook-Pro.local>
2019-06-29 00:14:13 +0300

    Added function AddNumber

--- code.js
+++ code.js
@@ -2,6 +2,10 @@ function GetName(Name, LastName) {
   document.write(Name + ' ' +LastName);
 }
 
+function addNumber(number1, number2) {
+  return number1+number2;
+}
+
 // var i = 1;
 // while (i < 5) {
 //   GetName('Ivan', 'Ivanov');
