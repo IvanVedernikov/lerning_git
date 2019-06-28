6bf593741ed6b3640cd6748dacb95662662abe89
Vedernikov Ivan <ivedernikov@ivedernikov-MacBook-Pro.local>
2019-06-29 00:05:35 +0300

    Start lerning GIT

---
+++ code.js
@@ -0,0 +1,21 @@
+function GetName(Name, LastName) {
+  document.write(Name + ' ' +LastName);
+}
+
+// var i = 1;
+// while (i < 5) {
+//   GetName('Ivan', 'Ivanov');
+//   i++;
+// }
+
+var org = {
+  colore: 'green',
+  height: 5,
+  weight: 180
+}
+
+document.write(org.colore);
+
+// for (var i = 0; i < 5; i++) {
+//     GetName('Ivan', 'Ivanov');
+// }
