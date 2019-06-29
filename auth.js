c007b23df3ecd34457f7e4493dc1b64b423d6eb6
Ivan Vedernikov <ivedernikov@me.com>
2019-06-29 10:59:45 +0300

    Inplementeted auth

---
+++ auth.js
@@ -0,0 +1,7 @@
+function authenticate(login, password) {
+  if (login === 'login' && password === 'pasword') {
+    return 'You were is logged in';
+  } else {
+    return 'Login is not correct';
+  }
+}
